6bd6e4de56bbc5dd8c93091c12dce9d6de665946
QuadriniL <#cLAUDIO0>
2022-08-05 19:54:30 -0300

    Payment entity

---
+++ src/entities/Payment.ts
@@ -0,0 +1,16 @@
+/**
+ * Class of Bill entity
+ * is used by services that generate a payment order
+ */
+class Payment {
+  /**
+   * @constructor Create a bill instance
+   * @param {Costumer} payer - Payer of payment
+   * @param {number} installments - Number of parcels of payment
+   * @param {string} issuer_id - Domain Company identificator of Mercado Pago
+   * @param {string} payment_method - Method of payment
+   * @param {string} token - Token of payment
+   * @param {number} transaction_amount - Amount of payment
+   */
+  constructor() {}
+}
